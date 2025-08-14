e140d12df9795614702b3c9e4573b76c6dcf7c44
EliranBe <eliranbenbenisti@gmail.com>
2025-08-14 15:43:25 +0300

    Update deepgram.js הוספה  detect_language: true, והסרה של en-US

--- deepgram.js
+++ deepgram.js
@@ -21,7 +21,7 @@ function startWebSocketServer(server) {
     let deepgram = deepgramClient.listen.live({
       model: 'nova-3',
       smart_format: true,
-      language: 'en-US',
+       detect_language: true,
       punctuate: true,
       interim_results: true,
       endpointing: 500,
@@ -89,7 +89,7 @@ ws.on('message', (message) => {
     deepgram = deepgramClient.listen.live({
       model: 'nova-3',
       smart_format: true,
-      language: 'en-US',
+       detect_language: true,
       punctuate: true,
       interim_results: true,
       endpointing: 500,
