2daf46966e438a57f232426972e29cf592197010
EliranBe <eliranbenbenisti@gmail.com>
2025-08-14 15:04:38 +0300

    Update deepgram.js הוספת גרשיים למילה TRUE ב

--- deepgram.js
+++ deepgram.js
@@ -21,7 +21,7 @@ function startWebSocketServer(server) {
     let deepgram = deepgramClient.listen.live({
       model: 'nova-3',
       smart_format: true,
-      detect_language: true,
+      detect_language: "true",
       language: 'en-US',
       punctuate: true,
       interim_results: true,
@@ -90,7 +90,7 @@ ws.on('message', (message) => {
     deepgram = deepgramClient.listen.live({
       model: 'nova-3',
       smart_format: true,
-      detect_language: true,
+      detect_language: "true",
       language: 'en-US',
       punctuate: true,
       interim_results: true,
