b6934aadc970596726c5412bbb416208f4893b9f
EliranBe <eliranbenbenisti@gmail.com>
2025-08-16 16:46:32 +0300

    Update deepgram.js

--- deepgram.js
+++ deepgram.js
@@ -35,8 +35,8 @@ function startWebSocketServer(server) {
         interim_results: true,
         endpointing: 100,
         vad_events: true,  
-        encoding: 'linear16',
-        sample_rate: 16000
+        encoding: 'opus',
+        sample_rate: 48000
       });
 
       readyToSendAudio = false;
