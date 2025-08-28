010e48c326ba0ac78705a164a414f656482f3830
EliranBe <eliranbenbenisti@gmail.com>
2025-08-28 11:15:58 +0300

    Update Azure-translator.js

--- Azure-translator.js
+++ Azure-translator.js
@@ -1,1 +1,20 @@
+const WebSocket = require('ws');
+const express = require('express'); // אם צריך app
+const TextTranslationClient = require("@azure-rest/ai-translation-text").default,
+  { isUnexpected } = require("@azure-rest/ai-translation-text");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+module.exports = function startWebSocketServer(server, app) {
+  const wss = new WebSocket.Server({ server }); // server מגיע מ-index.js
+
+const endpoint = process.env["AZURE_TRANSLATOR_ENDPOINT"];
+const resourceId = process.env["AZURE_TRANSLATOR_RESOURCE_ID"];
+const region = process.env["AZURE_TRANSLATOR_REGION"];
+const AZURETRANSLATORApiKEY1 = process.env["AZURE_TRANSLATOR_KEY1"];
+
+
+  
+}
+
+
 
