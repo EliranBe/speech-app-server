21e12ab2044fd0984d88d36db98d63c7bd7c637a
EliranBe <eliranbenbenisti@gmail.com>
2025-08-29 14:07:32 +0300

    Update azure-translator.js

--- azure-translator.js
+++ azure-translator.js
@@ -1,36 +1,45 @@
 const axios = require("axios");
- 
+
 const subscriptionKey = process.env.AZURE_TRANSLATOR_KEY1; // המפתח הישיר
 const endpoint = process.env["AZURE_TRANSLATOR_ENDPOINT"];
 const region = process.env["AZURE_TRANSLATOR_REGION"];
 
 // פונקציה כללית לתרגום טקסט
-async function translateText(inputText, from, to) {
+async function translateText(inputText) {
    if (!subscriptionKey) {
     throw new Error("⚠️ Missing AZURE_TRANSLATOR_KEY1 in environment variables");
   }
+
+const url = `${endpoint}/translate`;
+  const params = {
+    "api-version": "3.0",
+    from: "en",
+    to: "he,ar"
+  };
+  const headers = {
+    "Ocp-Apim-Subscription-Key": subscriptionKey,
+    "Ocp-Apim-Subscription-Region": region,
+    "Content-Type": "application/json",
+  };
+
    try {
-    const response = await axios({
-      baseURL: endpoint,
-      url: "/translate",
-      method: "post",
-      headers: {
-        "Ocp-Apim-Subscription-Key": subscriptionKey,
-        "Ocp-Apim-Subscription-Region": region,
-        "Content-Type": "application/json",
-      },
-      params: {
-        "api-version": "3.0",
-        from: "en",
-        to: "he",
-        },
-      data: [{ Text: inputText }],
-      responseType: "json",
+    const response = await axios.post(url, [{ Text: inputText }], { params, headers });
+
+    // response.data[0] הוא הטקסט הראשון
+    const translations = response.data?.[0]?.translations || [];
+
+    // לולאה על כל התרגומים
+    const results = {};
+    translations.forEach(t => {
+      results[t.to] = t.text; // key = שפת יעד, value = הטקסט המתורגם
     });
-    return response.data?.[0]?.translations?.[0]?.text || "";
+
+    return results;
+       
   } catch (err) {
 console.error("❌ Translation API error:", err.message);
-    throw err;  }
+       return {};
+      }
 }
 
 module.exports = { translateText };
