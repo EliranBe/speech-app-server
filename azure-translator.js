e336300ea442414f0b1d9bb5a390c9c742f371c6
EliranBe <eliranbenbenisti@gmail.com>
2025-08-29 08:26:03 +0300

    Update azure-translator.js

--- azure-translator.js
+++ azure-translator.js
@@ -5,34 +5,27 @@ const endpoint = process.env["AZURE_TRANSLATOR_ENDPOINT"];
 const region = process.env["AZURE_TRANSLATOR_REGION"];
 
 // פונקציה כללית לתרגום טקסט
-async function translateText(inputText, from, to) {
-   if (!subscriptionKey) {
-    throw new Error("⚠️ Missing AZURE_TRANSLATOR_KEY1 in environment variables");
-  }
+
+async function translateText(text) {
+        const baseURL= endpoint;
+const url = `${endpoint}/translate`;
+  const params = {
+    'api-version': '3.0',
+    from: 'en',
+    to: ['he', 'ar']  // Axios ימיר את זה ל-to=he&to=ar
+  };
+
+  const headers = {
+    'Ocp-Apim-Subscription-Key': subscriptionKey,
+    'Ocp-Apim-Subscription-Region': region,
+    'Content-Type': 'application/json'
+  };
 
   try {
-    const response = await axios({
-      baseURL: endpoint,
-      url: "/translate",
-      method: "post",
-      headers: {
-        "Ocp-Apim-Subscription-Key": subscriptionKey,
-        "Ocp-Apim-Subscription-Region": region,
-        "Content-Type": "application/json",
-      },
-      params: {
-        "api-version": "3.0",
-        from: "en",
-        to: ["he", "ar"],
-        },
-      data: [{ Text: inputText }],
-      responseType: "json",
-    });
-
-return response.data?.[0]?.translations || [];
+    const response = await axios.post(url, [{ Text: text }], { params, headers });
+    return response.data;
   } catch (err) {
-    console.error("❌ Translation API error:", err.message);
-    throw err;
+    console.error('Translation error:', err.response?.data || err.message);
   }
 }
 
