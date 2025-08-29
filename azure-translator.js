000e734283a16dd049130ae8d88ec39e3dbe0b5b
EliranBe <eliranbenbenisti@gmail.com>
2025-08-29 08:14:58 +0300

    Update azure-translator.js

--- azure-translator.js
+++ azure-translator.js
@@ -23,14 +23,13 @@ async function translateText(inputText, from, to) {
       params: {
         "api-version": "3.0",
         from: "en",
-        to: "he",
-        to: "ar",
-      },
+        to: ["he", "ar"],
+        },
       data: [{ Text: inputText }],
       responseType: "json",
     });
 
-    return response.data?.[0]?.translations?.[0]?.text || "";
+return response.data?.[0]?.translations || [];
   } catch (err) {
     console.error("❌ Translation API error:", err.message);
     throw err;
