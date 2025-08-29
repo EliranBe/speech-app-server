c2ec3bcbc6b60aed6aff8a24fccad1767ca58bed
EliranBe <eliranbenbenisti@gmail.com>
2025-08-29 17:53:26 +0300

    Update azure-translator.js

--- azure-translator.js
+++ azure-translator.js
@@ -13,8 +13,8 @@ async function translateText(inputText) {
 const url = `${endpoint}/translate`;
   const params = {
     "api-version": "3.0",
-    from: "en",
-    to: "he,ar"
+    from: "ru",
+    to: "he,ar,en"
   };
   const headers = {
     "Ocp-Apim-Subscription-Key": subscriptionKey,
