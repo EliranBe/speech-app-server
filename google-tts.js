1a3767485cee8488c70e255831204cb37d98aa18
EliranBe <eliranbenbenisti@gmail.com>
2025-09-04 15:44:51 +0300

    Update google-tts.js

--- google-tts.js
+++ google-tts.js
@@ -13,7 +13,7 @@ const client = new textToSpeech.TextToSpeechClient({
  * @param {string} [voiceName] - שם הקול הספציפי ב-Google TTS, למשל 'he-IL-Wavenet-B'
  * @returns {Promise<string>} Base64 של האודיו
  */
-async function synthesizeTextToBase64(text, languageCode = 'en-US', gender = 'FEMALE', voiceName = "en-US-Chirp3-HD-Sulafat") {
+async function synthesizeTextToBase64(text, languageCode = 'es-ES', gender = 'MALE', voiceName = "es-ES-Chirp3-HD-Schedar") {
   const request = {
     input: { text },
     voice: { 
