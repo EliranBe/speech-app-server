3586666cf3dd893190ed2f54c9b683189b1b230c
EliranBe <eliranbenbenisti@gmail.com>
2025-09-11 15:47:34 +0300

    Create App.js

---
+++ client/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "@/Pages/Home";
+import Preferences from "@/Pages/Preferences";
+import Login from "@/Pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/Preferences" element={<Preferences />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
