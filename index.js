4322abfa178ff7cd3709cffd62285c6bff3f6539
EliranBe <eliranbenbenisti@gmail.com>
2025-09-11 12:06:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const dotenv = require("dotenv");
 dotenv.config();
+const supabase = require('./utils/supabaseClient');
 const cors = require('cors');
 
 // בדיקה ש־Google TTS מוגדר
