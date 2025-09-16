1c84c5e76628974fe587c8ebbdcb2c9237454a78
EliranBe <eliranbenbenisti@gmail.com>
2025-09-16 22:51:56 +0300

    Update supabaseClient.js

--- client/src/utils /supabaseClient.js
+++ client/src/utils /supabaseClient.js
@@ -2,8 +2,8 @@
 // client/src/utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.APP_SUPABASE_URL;
-const supabaseKey = process.env.APP_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
