15bd7c6756b64dfbe1e2916a3e9294c0156b9d10
EliranBe <eliranbenbenisti@gmail.com>
2025-09-26 17:58:58 +0300

    Update Preferences.js

--- client/src/Pages/Preferences.js
+++ client/src/Pages/Preferences.js
@@ -216,7 +216,7 @@ export default function Preferences() {
             style={{
               width: "140px",
               height: "140px",
-              marginBottom: "1rem",
+              marginBottom: "0 auto 1.5rem auto",
               cursor: "pointer",
             }}
             onClick={() => navigate("/")}
