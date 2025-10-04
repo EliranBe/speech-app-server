f4bc9a4f214adf5d7d7216bc94a55fb50f6eb1bb
EliranBe <eliranbenbenisti@gmail.com>
2025-10-04 21:47:59 +0300

    Update meetings.js

--- server/meetings.js
+++ server/meetings.js
@@ -271,7 +271,7 @@ if ((!meetingRow || !meetingRow.meeting_id) && url_meeting) {
     const urlMatch = url_meeting && meetingRow.url_meeting === url_meeting;
     const passwordMatch =
       meeting_id && meeting_password &&
-      meetingRow.meeting_id === meeting_id &&
+      meetingRow.meeting_id === meeting_id_to_use &&
       meetingRow.meeting_password === meeting_password;
 
     if (!(urlMatch || passwordMatch)) {
