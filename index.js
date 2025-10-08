f1e4233b8b188db22cb2396f1dc36bf8845d982b
EliranBe <eliranbenbenisti@gmail.com>
2025-10-08 10:26:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const dotenv = require("dotenv");
 dotenv.config();
-const { supabase } = require('./client/src/utils/supabaseClient');
+const { supabase } = require('./server/utils/supabaseClient');
 const cors = require('cors');
 const meetingsRouter = require("./server/meetings");
 const checkBlockedUser = require("./server/middleware/checkBlockedUser");
